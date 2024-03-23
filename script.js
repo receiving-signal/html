8f014b452a7d1ac3959bde128021e60054fa30e5
LaboratoryManiac <writeleft1@gmail.com>
2024-03-24 03:46:12 +1100

    sound revert

--- script.js
+++ script.js
@@ -1,35 +1,10 @@
-'speechSynthesis' in window ? console.log("Web Speech API supported!") : console.log("Web Speech API not supported :-(");
-const synth = window.speechSynthesis;
-let ourText = "";
-const utterThis = new SpeechSynthesisUtterance(ourText);
-utterThis.pitch = 0;
-for (const voice of synth.getVoices()) {
-  if (voice.name === "Microsoft James - English (Australia) (en-AU)") {
-    utterThis.voice = voice;
-  }
-}
-audioStatic = new Audio('static.wav');
-audioStatic.loop = true;
-
-audioStatic.play();
 setInterval(update, 5000);
 
 function update() {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function() {
-    ourText = this.responseText;
-    document.getElementById("demo").innerHTML = ourText;
-    if (ourText == ".") {
-      playSound("tone");
-    } else {
-      utterThis.text = ourText;
-      synth.speak(utterThis);
-    }
+    document.getElementById("demo").innerHTML = this.responseText;
   }
   xhttp.open("GET", "crypt.txt", true); //import from py
   xhttp.send();
 }
-
-function playSound(name) {
-  document.getElementById(name).play();
-} 
