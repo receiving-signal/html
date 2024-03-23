3660887d07e0878c847c68fc245ac20b8c5efbb4
LaboratoryManiac <writeleft1@gmail.com>
2024-03-24 02:29:27 +1100

    synth speech test1

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-setInterval(function () {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("demo").innerHTML = this.responseText;
-      }
-    xhttp.open("GET", "crypt.txt", true); //import from py
-    xhttp.send();
-  }, 5000);
+'speechSynthesis' in window ? console.log("Web Speech API supported!") : console.log("Web Speech API not supported :-(")
+const synth = window.speechSynthesis
+let ourText = ""
+const utterThis = new SpeechSynthesisUtterance(ourText)
+
+setInterval(update, 5000);
+
+function update() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("demo").innerHTML = this.responseText;
+    }
+  xhttp.open("GET", "crypt.txt", true); //import from py
+  xhttp.send();
+
+  utterThis.text = this.responseText;
+  synth.speak(utterThis)
+}
